78cb167a95554ebfce6a5f1c53ad1382d416b957
evro <evandanrosa@gmail.com>
2025-04-01 23:45:43 -0400

    updated for deploy take 5

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -1,10 +1,14 @@
+// app/api/posts/[slug]/route.ts
 export const runtime = 'nodejs'
 
 import { getPostBySlug } from '@/lib/api'
 import { NextResponse } from 'next/server'
 
-export async function GET(req: Request, { params }: { params: { slug: string } }) {
-    const post = getPostBySlug(params.slug)
+export async function GET(
+    req: Request,
+    context: { params: { slug: string } }
+) {
+    const post = getPostBySlug(context.params.slug)
 
     if (!post) {
         return NextResponse.json({ error: 'Not found' }, { status: 404 })
